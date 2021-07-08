8b08d1bf78ca336b2f408fdccb8108b6a4663374
Ferchbo <ferchboo@gmail.com>
2021-07-08 05:17:36 +0000

    https://github.com/Ferchbo/vftvk-Simple-Interest-Calculator.git

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
 function compute()
 {
+    var principal = document.getElementById("Principal").value;
+    var rate = document.getElementById("rate").value;
+    var years = document.getElementById("years").value;
+    var var interest = principal * years * rate /100;
+    var year = new Date().getFullYear()+parseInt(years);
     p = document.getElementById("principal").value;
-    
+}
+function updaterate()
+{
+    var rateval = document.getElementById("rate").value;
+    document.getElementById("rate_val").innerText=rateval;
 }
         
